54e0f002cc49eadddc21711e977221feacf59a4d
David <odmh2711@gmail.com>
2022-12-13 17:35:31 -0500

    feature! #5: Add new functionality to delete selected files

--- utils/core.py
+++ utils/core.py
@@ -16,23 +16,15 @@ from .variables import (
 def delete_files() -> None:
     """
         Function that deletes all the downloaded files when called
-
-        Params
-        downloads_folder The path that cointains all the downloaded files
-
-        Return
-        None
     """
     for filename in LIST_OF_FILES:
-        # .ini files contain information about how Windows display the folder
-        if not filename.endswith('.ini'):
-            os.remove(path=PATH + filename)
+        os.remove(path=PATH + filename)
 
 #### Tkinter functionality ####
 
 def display_main_window() -> None:
     """
-        Function that will set the Tkinter main window
+        Function that will set and display the Tkinter main window
     """
     master.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{SCREEN_WIDTH}+{SCREEN_HEIGHT}")
     master.title("Klener")
@@ -46,19 +38,12 @@ def display_sec_window() -> Toplevel:
         Return
             A Tkinter's Toplevel object with the secondary window
     """
-    def close_windows() -> None:
-        """
-            Function that will close recently opened Tkinter windows
-        """
-        window.destroy()
-        master.destroy()
-
     window = Toplevel(master)
     window.geometry(f"200x170+{SCREEN_WIDTH + 80}+{SCREEN_HEIGHT + 40}")
     window.title("Info")
     window.resizable(width=False, height=False)
     
-    close_btn = Button(window, text="Finish", command=close_windows)
+    close_btn = Button(window, text="Finish", command=master.destroy)
     close_btn.pack(side=BOTTOM, pady=40)
 
     return window
@@ -66,7 +51,7 @@ def display_sec_window() -> Toplevel:
 
 def delete_all() -> None:
     """
-        Function that will call the delete_files function and display information about it
+        Function that will call the delete_files function and display information about the process
     """
     info_window: Toplevel = display_sec_window()
     delete_files() # Deleting all files...
@@ -88,13 +73,12 @@ def display_main_btns() -> None:
     """
         Function that will create and display the buttons for interaction on the Tkinter's main window
     """
-    # This will be the button that deletes all the existing files in the Downloads folder
+    # Button that deletes all existing downloaded files
     delete_btn = Button(master, text="Delete all", command=delete_all)
     delete_btn.place(x=80, y=(WINDOW_HEIGHT - 60))
 
-
-    # This will be the 'No' option
-    select_btn = Button(master, text="Select what to delete", state=DISABLED)
+    # Button to select certain files to delete
+    select_btn = Button(master, text="Select what to delete", command=create_sf_window)
     select_btn.place(x=160, y=(WINDOW_HEIGHT - 60))
 
 
@@ -105,3 +89,83 @@ def display_okclose_btn() -> None:
     """
     okclose_btn = Button(master, text="Ok, close", command=master.destroy)
     okclose_btn.pack(side=BOTTOM, pady=30)
+
+#### ONLY SELECTED FILES FUNCTIONALITY ####
+
+def display_sf_second_window(sfwindow: Toplevel) -> Toplevel:
+    """
+        Function that will create and display a new information window with the status of the
+        selected files deletion
+
+        Return
+            A Tkinter's Toplevel object with the secondary window
+    """
+    window: Toplevel = Toplevel(sfwindow)
+    window.geometry(f"200x170+{SCREEN_WIDTH + 50}+{SCREEN_HEIGHT + 40}")
+    window.title("Info")
+    window.resizable(width=False, height=False)
+    
+    # Close information window button
+    close_btn = Button(window, text="Finish", command=master.destroy)
+    close_btn.pack(side=BOTTOM, pady=40)
+
+    return window
+
+
+def create_sf_window() -> None:
+    """
+        Function that creates and displays on Tkinter the interface for the user to delete
+        only certain files
+    """
+    # Main window
+    selectfiles_window = Toplevel(master)
+    selectfiles_window.geometry(f"{WINDOW_WIDTH + 100}x{WINDOW_HEIGHT + 100}+{SCREEN_WIDTH - 80}+{SCREEN_HEIGHT - 50}")
+    selectfiles_window.title("Select what to delete")
+    selectfiles_window.resizable(width=False, height=False)
+
+    # Main label setting
+    label: Label = Label(selectfiles_window, text="Select what to delete")
+    label.pack(side=TOP, pady=10)
+
+    # Scrollbar setting
+    scrollbar = Scrollbar(selectfiles_window)
+    scrollbar.pack(side=RIGHT, fill=Y)
+
+    # Create a Listbox
+    listbox = Listbox(selectfiles_window, selectmode=MULTIPLE, yscrollcommand=scrollbar.set)
+    listbox.pack(side=TOP, expand=YES, fill=BOTH, padx=10, pady=70)
+
+    # Insert items into the Listbox
+    for index, item in enumerate(LIST_OF_FILES):
+        listbox.insert(index, item)
+
+    # Exit button
+    exit_btn = Button(selectfiles_window, text="Close", command=selectfiles_window.destroy)
+    exit_btn.place(x=180, y=(WINDOW_HEIGHT + 60))
+
+    def remove_selected_files() -> None:
+        """
+            Function that deletes the selected files
+        """
+        # Get the selected elements from the Listbox
+        selected_items = listbox.curselection()
+
+        if selected_items:
+            # Create a new information window
+            info_window = display_sf_second_window(selectfiles_window)
+
+            for index_of_file in selected_items:
+                # Selected files deletion
+                os.remove(PATH + LIST_OF_FILES[index_of_file])
+
+            info_label = Label(info_window, text="Files deleted")
+            info_label.pack(pady=20)
+            
+
+    # Delete selected files button
+    delete_btn = Button(selectfiles_window, text="Delete", command=remove_selected_files)
+    delete_btn.place(x=100, y=(WINDOW_HEIGHT + 60))
+
+    # Delete all files button
+    delete_anyway_btn = Button(selectfiles_window, text="Delete all anyway", command=delete_all)
+    delete_anyway_btn.place(x=260, y=(WINDOW_HEIGHT + 60))
--- utils/variables.py
+++ utils/variables.py
@@ -4,20 +4,20 @@ import os
 
 master = Tk()
 
-# Tkinter's window dimensions
+# Tkinter main window dimensions
 WINDOW_WIDTH = 350
 WINDOW_HEIGHT = 200
 
-# User screen dimensions
+# Tkinter main window dimensions
 SCREEN_HEIGHT = int(master.winfo_screenheight() / 2) - int(WINDOW_HEIGHT / 2)
 SCREEN_WIDTH = int(master.winfo_screenwidth() / 2) - int(WINDOW_WIDTH / 2)
-
 
 USER_USERNAME: str = os.environ.get('USERNAME')
 
 PATH: str = f"C:/Users/{USER_USERNAME}/Downloads/"
 
-LIST_OF_FILES: list = os.listdir(PATH)
+# .ini files contain information about how Windows display the folder
+LIST_OF_FILES: list = list(filter(lambda filename: filename != "desktop.ini", os.listdir(PATH)))
 
 # Quantity of files in the Downloads folder
-FILE_NUM: int = len(LIST_OF_FILES) - 1 # I substract 1 because of the .ini file
+FILE_NUM: int = len(LIST_OF_FILES)
